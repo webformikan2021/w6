1ce17208eb28c843fc7ccc0eb5ab44d809b42e0f
webformikan2021 <95006565+webformikan2021@users.noreply.github.com>
2022-01-07 12:43:36 +0900

    Update main.js

--- homework/main.js
+++ homework/main.js
@@ -1,22 +1,23 @@
+// Personalized welcome message code
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
-
+console.log(myHeading);//追加
 function setUserName() {
   let myName = prompt('名前を入力してください');
   if(!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = '今日は' + myName+'さん';
+    myHeading.innerHTML = 'こんにちは, ' + myName; //Mozilla is cool,から「こんにちは」に変更
   }
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
-  let storedName = localStorage.getItem('仮名称');
-  myHeading.innerHTML = '今日は' + storedName+'さん';
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'こんにちは, ' + storedName; 
 }
 
 myButton.onclick = function() {
