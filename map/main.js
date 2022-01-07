ac4a1b23360d83b3aa179bb9c717cc71f2ba39a7
webformikan2021 <95006565+webformikan2021@users.noreply.github.com>
2022-01-07 11:41:04 +0900

    Create main.js

---
+++ map/main.js
@@ -0,0 +1,24 @@
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
