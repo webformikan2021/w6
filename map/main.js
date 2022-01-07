f8787ca1e2464c645fc3d1bfe5b182e59b947a5e
webformikan2021 <95006565+webformikan2021@users.noreply.github.com>
2022-01-07 11:55:10 +0900

    Update main.js

--- map/main.js
+++ map/main.js
@@ -3,20 +3,20 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-  let myName = prompt('Please enter your name.');
+  let myName = prompt('名前を入力してください');
   if(!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    myHeading.innerHTML = '今日は' + myName+'さん';
   }
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
-  let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  let storedName = localStorage.getItem('仮名称');
+  myHeading.innerHTML = '今日は' + storedName+'さん';
 }
 
 myButton.onclick = function() {
